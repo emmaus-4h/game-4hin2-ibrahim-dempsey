52185540af5e718a96e5e4c03e29a1751b74be82
iibyk <92523506+iibyk@users.noreply.github.com>
2022-05-11 12:36:02 +0000

    map

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /* Game opdracht
    Informatica - Emmauscollege Rotterdam
    Template voor een game in JavaScript met de p5 library
@@ -14,10 +15,16 @@ const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
-var spelerX = 600; // x-positie van speler
-var spelerY = 600; // y-positie van speler
-var vijandX = 600;
-var vijandY = 500;
+var spelerX = 900; // x-positie van speler
+var spelerY = 680; // y-positie van speler
+var vijandX = 300;
+var vijandY = 680;
+var platformX = 300;
+var platformY = 280;
+var platform2X = 100;
+var platform2Y = 300;
+var platform3X = 880; // 1280(breedte) - 100(gat) - 300(balkbreedte)
+var platform3Y = 300;
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -28,18 +35,31 @@ var vijandY = 500;
  */
 var beweegAlles = function () {
   // speler
-  if (keyIsDown(65)) {
+  if (keyIsDown(37)) {
   spelerX = spelerX - 7;
   }
-  if (keyIsDown(68)) {
+  if (keyIsDown(39)) {
   spelerX = spelerX + 7;
   }
-  if (keyIsDown(32)) {
+  if (keyIsDown(38)) {
   spelerY = spelerY - 7;
   }
   // vijand
+  if (keyIsDown(68)) {
+  vijandX = vijandX + 7;
+  }
+  if (keyIsDown(65)) {
+  vijandX = vijandX - 7;
+  }
+    if (keyIsDown(87)) {
+  vijandY = vijandY - 7;
+  }
 
   // kogel
+ 
+  // mechanic
+
+  
 };
 
 /**
@@ -69,16 +89,25 @@ var tekenAlles = function () {
 fill (117, 147, 217)
   rect(0,0,1280,720)
   // vijand
-  fill("red");
-  rect(vijandX - 25, vijandY - 25, 50, 50);
+  fill(42, 140, 0);
+  rect(vijandX - 80/2, vijandY - 160, 90, 160);
+  fill(0,0,0);
+  ellipse(vijandX, vijandY, 10,10);
   // kogel
 
   // speler
-  fill("white");
-  rect(spelerX - 25, spelerY - 25, 50, 50);
+  fill(140, 0, 30);
+   rect(spelerX - 80/2, spelerY - 160, 80, 160);
+  fill(0,0,0);
+  ellipse(spelerX, spelerY, 10,10);
 
   // punten en health
 
+  // map & mechanic
+fill(140, 0, 200);
+  rect(platformX - 1000, platformY + 400, 10000, 50);
+  rect(platform2X, platform2Y, 300, 20);
+  rect(platform3X , platform3Y , 300, 20);
 };
 
 /**
